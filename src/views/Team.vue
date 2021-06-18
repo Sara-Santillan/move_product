3fc19cbd18ab5067c5f7db56e0d46627b46ee9ab
sara <sarasantillan@gmail.com>
2021-06-18 18:24:59 +0800

    fetching of data from API

--- src/views/Team.vue
+++ src/views/Team.vue
@@ -1,4 +1,36 @@
 <template lang="pug">
 .about
     h1 This is the team view
+    p {{ members }}
 </template>
+
+<script>
+import TeamMemberList from '@/components/TeamMemberList.vue'
+
+export default {
+    name: 'Team',
+    components: {
+        TeamMemberList
+    },
+    // we will add methods to FETCH data from an API &
+    // pass it into data, to execute in the setup hook:
+    data () {
+        return {
+            members: []
+        }
+    },
+    methods: {
+        async fetchMembers () {
+            const url = 'https://randomuser.me/api/?results=4'
+            await fetch(url)
+                .then(response => response.json())
+                .then(data => {
+                    this.members = data.results
+                })
+        }
+    },
+    mounted () {
+        this.fetchMembers()
+    }
+}
+</script>
